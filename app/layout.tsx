c33cbbc39fd9112aed91a11c3335cdda67c784ce
RafaelSillva <rafaelconceicao822@gmail.com>
2024-11-13 23:18:52 -0400

    feat: add icon for website and renderizer

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/piggy-bank-solid.svg" />
+      </head>
       <body className={`${mulish.className} dark antialiased`}>
         <ClerkProvider
           appearance={{
